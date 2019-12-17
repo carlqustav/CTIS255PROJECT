c87eab35834cc420b75155b5061c56107fde6524
carlqustav <berksngl@gmail.com>
2019-12-18 00:13:45 +0300

    again

--- memory.js
+++ memory.js
@@ -1,8 +1,56 @@
 
+var zorluk = 0;
+var tempW = 0;
+var tempH = 35;
+var count = 1;
+const mainW = 820;
+const mainH = 700;
+
+var rndArea = [];
+var selected = 0;
+
+for(var i = 0; i < 4 ; i++){
+    rndArea[i] = [];
+    for(var j = 0; j < 4 ; j++){
+        rndArea[i][j] = false;
+    }
+}
+
+while(selected != 4){
+    var i = parseInt(Math.random()*3+1);
+    var j = parseInt(Math.random()*3+1);
+
+    if(!rndArea[i][j]){
+        rndArea[i][j] = true;
+        selected++;
+    }
+}
+
 $(function(){
+
     $("button").click(function() {
+
+        zorluk = diff.dif.value;        
+
         $("#cover").fadeOut(1000);
-        $("#main").animate({"background-color":"rgba(0,0,0,0.6)"});
-        $("#main").animate({"box-shadow":"rgba(0,0,0,1)"});
+        $("#main").animate({"background-color":"rgba(0,0,0,0)"},'slow', function(){
+            $("#main").css({"background-image":`url("memorybackground.jpg")`});
+
+        });
+        
+        for(var i = 0; i < 4 ; i++){
+            tempW = 40;
+            for(var j = 0; j < 4 ; j++){
+                if(rndArea[i][j])
+                    $("#main").prepend(`<div class="white" style="left:${tempW}px;top:${tempH}px;"></div>`);
+                tempW += 220;
+            }
+            tempH += 180;      
+        }       
     });
+
+    $(".white").on("click",function(){
+        alert("osman");
+    });
+    
 });
