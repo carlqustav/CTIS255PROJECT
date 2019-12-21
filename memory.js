152d052421e197f2355424fd00397d37b82d33b1
carlqustav <berksngl@gmail.com>
2019-12-21 21:49:16 +0300

    az az kaldı
    oyun bitmeden basılmış kutularda hala event var

--- memory.js
+++ memory.js
@@ -1,7 +1,6 @@
-
 var zorluk = 0;
 var tempW = 0;
-var currBox = 1;
+var currBox = 0;
 var tempH = 35;
 var count = 1;
 const mainW = 820;
@@ -29,8 +28,8 @@ $(function () {
 
         //Random box initialization
         while(selected != zorluk){
-            var i = parseInt(Math.random() * 3 + 1);
-            var j = parseInt(Math.random() * 3 + 1);
+            var i = parseInt(Math.random() * 4);
+            var j = parseInt(Math.random() * 4);
             
             if (!rndArea[i][j]) {
                 rndArea[i][j] = true;
@@ -60,35 +59,38 @@ $(function () {
             $("#game").fadeIn(1000);
         });
         
-
         //Grading Criteria 5 : Random boxes 
         for (var i = 0; i < 4; i++) {
             tempW = 40;
             for (var j = 0; j < 4; j++) {
                 if (rndArea[i][j]) {
-                    var tempDiv = $(`<div class="white" style="display:none;left:${tempW}px;top:${tempH}px;">${++selected}</div>`);
+                    var tempDiv = $(`<div class="white" style="left:${tempW}px;top:${tempH}px;">
+                                        <div class="number" style="display:none;left:${tempW}px;top:${tempH}px;">
+                                            ${divRnd[selected]}
+                                        </div>
+                                     </div>`);
                         
                     $("#game").prepend(tempDiv);
-
-                    div.push(tempDiv);
+                    
+                    div[divRnd[selected]-1] = tempDiv;
+                    selected++;
                 }
                 tempW += 220;
             }
             tempH += 180;
-        }    
-
+        }   
         animateNumber(div,0);
-        
     });
-
 });
 
-//Grading Criteria 6: recursive function to create an animation stack
+//Grading Criteria 6: Recursive function to create an animation stack
 function animateNumber(div,i){
     if(i != div.length){
-        div[i].fadeIn(1000,function(){
-            div[i].delay(1000);
-            div[i].css({"color":"white"});
+
+        if(i == 0)
+            div[i].children().delay(2000);
+
+        div[i].children().fadeIn(1000).fadeOut(1000,function(){
             animateNumber(div,++i);
         }); 
     }
@@ -98,27 +100,32 @@ function animateNumber(div,i){
 }
 //Grading Criteria 7 : Sequence checking
 function whiteEventBinding(){
+
+    currBox = 1;
+
     $(function(){
         $("#game").on('click','div', function () { 
-
             if($(this).text() != currBox){
                 $("#main").after(`<div id="win"><p>You failed in level ${zorluk}. Press F5 to play again.</p></div>`);
                 $("#main").css({"box-shadow":"0px 0px 25px 0px red"});
                 $("#win").css({"box-shadow":"0px 0px 25px 0px red"});
-                $(this).css({"box-shadow":"0px 0px 25px 0px red"});
-                $(this).parent().children().off();
+                $(this).css({"box-shadow":"0px 0px 25px 0px red"})
+                        .css({"background":"red"});
+                $("#game").off('click');
             }
             else{
                 if(currBox  == zorluk){
                     $("#main").after(`<div id="win"><p>You completed level ${zorluk}. Press F5 to play again.</p></div>`);
                     $("#main").css({"box-shadow":"0px 0px 25px 0px green"});
                     $("#win").css({"box-shadow":"0px 0px 25px 0px green"});
-                    $(this).css({"box-shadow":"0px 0px 25px 0px green"});
-                    $(this).parent().children().off();
+                    $(this).css({"box-shadow":"0px 0px 25px 0px green"})
+                            .css({"background":"green"});
+                    $("#game").off('click');
                 }
                 else{
                     currBox++;
-                    $(this).css({"box-shadow":"0px 0px 25px 0px green"});
+                    $(this).css({"box-shadow":"0px 0px 25px 0px green"})
+                            .css({"background":"green"});
                 }
             }
         });
